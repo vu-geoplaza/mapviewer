f7a092bfbf75d5d5d6227623ae6ea05c314d6b6c
Peter Vos <pjmvos@gmail.com>
2019-02-06 13:37:16 +0100

    return layers from getCapabilities, rewrite merge

--- src/helpers/service/ViewerService.js
+++ src/helpers/service/ViewerService.js
@@ -13,39 +13,80 @@ class ViewerService {
       this.setLayers(config.layers);
     }
     console.log(this);
-  }
-
+  };
   async getServiceInstance(crs) {
     var me = this;
-    var layer_names = [];
-    for (const l of me.layers) {
-      layer_names.push(l.title);
-    }
     console.log('get service instance');
-    var service = await this.getCapabilities(layer_names).then(function (cservice) {
-      var i=0;
-      for (const layer of cservice.layers){
+    var service = await this.getCapabilities().then(function (clayers) {
+      if (me.layers.length > 0) {
+        me.layers = me.mergeLayers(me.layers, clayers);
+      } else {
+        me.layers = clayers;
+      }
+      var i = 0;
+      for (const layer of me.layers) {
         console.log(layer);
-        layer.setOL(cservice.url, crs);
+        layer.setOL(me.url, crs);
         if (layer.ol !== false) {
-          cservice.layers[i].ol=layer.ol;
+          me.layers[i].ol = layer.ol;
         } else {
           // unset
-          cservice.layers[i].splice(i,1);
+          me.layers[i].splice(i, 1);
         }
         i++;
       }
-      return cservice;
+      return me;
     });
-    return service;
+    return me;
   };
+
   async getCapabilities(layer_names) {
     // get overwritten in all the subclasses;
     return this;
   };
+
   setLayers(layers) {
     // get overwritten in all the subclasses;
   };
+  mergeLayers(layers,clayers) {
+    const complayers=[];
+    const compare = this.compareByTitle(layers, clayers);
+    for (const title in compare.t2) {
+      const cindex=compare.t2[title];
+      const index=compare.t1[title];
+      if (index > -1) {
+        const lyr=clayers[cindex];
+        lyr.id = layers[index].id;
+        lyr.label = layers[index].label
+        lyr.visible = layers[index].visible;
+        lyr.opacity = layers[index].opacity;
+        lyr.zindex = layers[index].zindex
+        complayers.push(lyr);
+      }
+    }
+    return complayers;
+  };
+  compareByTitle(arr1, arr2) {
+    //hmmm
+    let i = 0;
+    const t1 = [];
+    for (const a1 of arr1) {
+      t1[a1.title] = i;
+      i++;
+    }
+    i = 0;
+    const t2 = [];
+    for (const a2 of arr2) {
+      if (a2.title in t1) {
+        t2[a2.title] = i;
+      }
+      i++;
+    }
+    return {
+      t1: t1, // indices of arr1
+      t2: t2 // intersection + indices of arr2
+    }
+  }
 }
 
 export default ViewerService;
