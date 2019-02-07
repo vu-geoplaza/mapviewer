5fc3b966775b147f7419b433c89bcc093c82c2f2
Peter Vos <pjmvos@gmail.com>
2019-02-07 17:49:59 +0100

    active projection, friendly names

--- src/components/ProjectionSwitcher/ProjectionSwitcher.vue
+++ src/components/ProjectionSwitcher/ProjectionSwitcher.vue
@@ -1,7 +1,7 @@
 <template>
   <b-nav-item-dropdown text="projection">
-    <b-dropdown-item v-on:click="switchProjection(item.name)" v-for="(item, index) in items" :index="index" :key="index" :item="item">
-      {{ item.name }}
+    <b-dropdown-item v-on:click="switchProjection(item.code)" v-for="(item, index) in items" :index="index" :key="index" :item="item">
+      {{ item.name }}&nbsp;{{ item.active }}
     </b-dropdown-item>
   </b-nav-item-dropdown>
 </template>
@@ -26,18 +26,32 @@
         var me = this;
         //me.initSwitcher();
         me.map.getLayers().on('change:length', function () {
-          me.initSwitcher();
+          me.initSwitcher(me.map.getView().getProjection().getCode());
         });
       },
-      initSwitcher() {
+      initSwitcher(current) {
         this.items=[];
         console.log('ínit proj switcher');
         for (const crs of this.map.available_crs) {
-          this.items.push({name: crs});
+          let name=crs;
+          if (crs==='EPSG:28992') { name='Amersfoort / RD New' }
+          if (crs==='EPSG:3857') { name='WGS 84 / Pseudo-Mercator' }
+          if (crs==='EPSG:4326') { name='World Geodetic System 1984' }
+          let active='';
+          if (crs===current){ active='*' }
+
+          this.items.push({name: name, code: crs, active: active});
         }
       },
       switchProjection(crs) {
         GpzEventBus.$emit('change-projection', crs);
+        for (const item in this.items){
+          if (item.code===crs) {
+            item.active='*'
+          } else {
+            item.active=''
+          }
+        }
         console.log('switch to' + crs);
       }
     },
