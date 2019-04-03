3ea3d8d414bf8633292a42d2ad06cc9f8bdb1fd1
Peter Vos <pjmvos@gmail.com>
2019-04-03 12:12:58 +0200

    info modal

---
+++ src/components/infomodal/InfoModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-modal id="infomodal" size="lg" title="Layers shown on this map">
+    <b-row>
+      <b-col md="12">
+      <ul v-for="service in services" :key="service">
+        <li>
+          <b>{{service.type}}</b>: {{service.url}}
+        </li>
+        <ul v-for="layer in service.layers" :key="layer">
+          <li>
+            {{layer.title}}
+          </li>
+        </ul>
+      </ul>
+      </b-col>
+    </b-row>
+  </b-modal>
+</template>
+
+<script>
+  export default {
+    name: 'LayerSwitcher',
+    data: function () {
+      return {
+        services: this.$config.services,
+      }
+    },
+  }
+</script>
+<style scoped>
+  #infomodal {
+    width: 80%;
+  }
+  li {
+    text-align: left;
+    word-wrap: break-word;
+  }
+</style>
