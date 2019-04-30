2375e1c688f43ce353dc32e6f4aa4a8384f2f6c9
Peter Vos <pjmvos@gmail.com>
2019-04-30 13:16:04 +0200

    correct RD def, set RD extent for tiled wms

--- src/main.js
+++ src/main.js
@@ -11,10 +11,12 @@ import ViewerConfig from './helpers/ViewerConfig'
 import axios from 'axios'
 
 import {register} from 'ol/proj/proj4'
+import {get as getProjection} from 'ol/proj'
 import proj4 from 'proj4';
 
-proj4.defs("EPSG:28992", "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs");
+proj4.defs("EPSG:28992", "+title=Amersfoort / RD New +proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +no_defs");
 register(proj4);
+getProjection('EPSG:28992').setExtent([-285401.92, 22598.08, 595401.92, 903401.92]);
 
 library.add(faArrowLeft, faArrowRight, faLayerGroup);
 
