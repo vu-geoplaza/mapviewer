e85c99ce59fe591953636f6c5c60116fa8bdd112
peer35 <p.j.m.vos@vu.nl>
2020-07-23 10:25:26 +0200

    url target

--- src/components/shared/navbar/NavBar.vue
+++ src/components/shared/navbar/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <b-navbar toggleable="md" type="dark" variant="info">
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-    <b-navbar-brand v-if="brand" :href="href">{{ title }}</b-navbar-brand>
+    <b-navbar-brand v-if="brand" :href="href" target="_blank">{{ title }}</b-navbar-brand>
     <b-collapse is-nav id="nav_collapse">
       <b-navbar-nav>
         <ProjectionSwitcher/>
