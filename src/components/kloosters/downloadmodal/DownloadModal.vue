818d347b535c33b071bf4aa918953205b78c0d93
peer35 <p.j.m.vos@vu.nl>
2021-03-15 11:41:36 +0100

    kloosterlijst migration

--- src/components/kloosters/downloadmodal/DownloadModal.vue
+++ src/components/kloosters/downloadmodal/DownloadModal.vue
@@ -5,7 +5,7 @@
         <h4>Over deze kaart</h4>
         <p>De Kloosterkaart toont de kloosters van alle orden die in Nederland gedurende de Middeleeuwen en de Vroegmoderne
         Tijd (tot 1800) vertegenwoordigd zijn geweest. De kaart is een visualisatie van de gegevens die zijn verzameld
-          in de Kloosterlijst: Beknopt overzicht van de Nederlandse kloosters tot 1800 (<a href="https://geoplaza.vu.nl/kloosterlijst/nl/">link</a>).</p>
+          in de Kloosterlijst: Beknopt overzicht van de Nederlandse kloosters tot 1800 (<a href="https://geoplaza.vu.nl/projects/kloosterlijst/nl/">link</a>).</p>
         <p><i>Meer informatie op de <a href="https://geoplaza.vu.nl/cms/research/kloosterkaart/">Geoplaza website</a></i></p>
         <h5>Download in het Nederlands</h5>
         <ul>
@@ -17,7 +17,7 @@
         <h4>About this map</h4>
         <p>The Map of Monasteries shows the monasteries of all orders which have been represented in the present-day
           Netherlands during the Middle Ages and Early Modern period (until 1800). The map visualises the data assembled
-          for the Census: Monasteries in the Netherlands until 1800 (<a href="https://geoplaza.vu.nl/kloosterlijst/en/">link</a>).</p>
+          for the Census: Monasteries in the Netherlands until 1800 (<a href="https://geoplaza.vu.nl/projects/kloosterlijst/en/">link</a>).</p>
         <p><i>More information on the <a href="https://geoplaza.vu.nl/cms/research/map-of-monasteries/">Geoplaza website</a></i></p>
         <h5>Download in English</h5>
         <ul>
