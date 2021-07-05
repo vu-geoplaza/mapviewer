6dae1e8eab83cdab1b842d6e0c7e31c35e3695ba
peer35 <p.j.m.vos@vu.nl>
2021-07-05 12:29:31 +0200

    fixes

--- src/helpers/kloosters/KloosterConfig.js
+++ src/helpers/kloosters/KloosterConfig.js
@@ -21,9 +21,11 @@ class KloosterConfig extends  ViewerConfig {
   }
 
   readJSON(json) {
-    if (json.klooster.info_url) this.klooster.info_url = json.klooster.info_url;
-    if (json.klooster.download_url) this.klooster.download_url = json.klooster.download_url;
-    if (json.klooster.symbol_url) this.klooster.symbol_url = json.klooster.symbol_url;
+    if (json.klooster){
+      if (json.klooster.info_url) this.klooster.info_url = json.klooster.info_url;
+      if (json.klooster.download_url) this.klooster.download_url = json.klooster.download_url;
+      if (json.klooster.symbol_url) this.klooster.symbol_url = json.klooster.symbol_url;
+    }
     super.readJSON(json)
   }
 
